411101c5d3444f222d1ace7213e0cee0ac5a5427
samrasugu <mokuasamr@gmail.com>
2022-08-24 00:01:22 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 
       <TimeAndLocation/>
       <TemperatureAndDetails/>
+      <Forecast/>
     </div>
   );
 }
